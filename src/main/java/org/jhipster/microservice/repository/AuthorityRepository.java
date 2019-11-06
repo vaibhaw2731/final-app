63d225923fe60af7827f01922fdc2dfd6c95ec9d
Vaibhaw Agrawal <vaibhaw.agrawal@publicissapient.com>
2019-11-06 12:48:09 +0530

    Initial version of ms01 generated by JHipster-6.4.1

---
+++ src/main/java/org/jhipster/microservice/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package org.jhipster.microservice.repository;
+
+import org.jhipster.microservice.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
